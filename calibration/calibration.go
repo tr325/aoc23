18c6e1f754869b841f7c7ee05120869490956faa
Tom Renner <tommrenner@gmail.com>
2023-12-05 22:23:33 +0100

    First star complete. Tests pass and calibration number extracted

--- calibration/calibration.go
+++ calibration/calibration.go
@@ -1,15 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func GetFirstAndLastNumbers(line string) (int, int) {
+	var first = -1
+	var last = -1
+
+	s := bufio.NewScanner(strings.NewReader(line))
+	s.Split(bufio.ScanRunes)
+
+	for s.Scan() {
+		i, err := strconv.Atoi(s.Text())
+		if err != nil {
+			// ignore
+		} else {
+			if first == -1 {
+				first = i
+			}
+			last = i
+		}
+	}
+
+	return first, last
+}
+
+func ConcatTwoDigits(first int, second int) int {
+	concat, _ := strconv.Atoi(strconv.Itoa(first) + strconv.Itoa(second))
+	return concat
+}
+
+func getSumOfFirstAndLastNumbers(line string) int {
+	first, last := GetFirstAndLastNumbers(line)
+	return ConcatTwoDigits(first, last)
+}
+
 func main() {
-    content, err := os.ReadFile("calibration.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(string(content))
+	readFile, _ := os.Open("calibration.txt")
+
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var sum = 0
+	for fileScanner.Scan() {
+		sum = sum + getSumOfFirstAndLastNumbers(fileScanner.Text())
+	}
+
+	fmt.Printf("Sum of all the calibration numbers: %d\n", sum)
 }
