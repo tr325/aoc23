a6ff000842052aa94b05b95cde2a76b8be2f0b77
Tom Renner <tommrenner@gmail.com>
2023-12-07 23:28:45 +0100

    Write part two, and add loads of tests to identify problem is in file parsing

--- gondola/gondola.go
+++ gondola/gondola.go
@@ -3,10 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/samber/lo"
 	"os"
 	"regexp"
 	"strconv"
-	"github.com/samber/lo"
 )
 
 type Part struct {
@@ -28,31 +28,39 @@ type SymbolData struct {
 }
 
 func parse(fileScanner *bufio.Scanner) ([]Part, []SymbolData) {
-	numberFinder := regexp.MustCompile(`[0-9]*`)
-	symbolFinder := regexp.MustCompile(`[^A-Za-z0-9\.]`)
 
 	var row = 0
 	parts := []Part{}
 	symbolData := []SymbolData{}
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		numbers := numberFinder.FindAllString(line, -1)
-		numberLocs := numberFinder.FindAllStringIndex(line, -1)
-		symbols := symbolFinder.FindAllString(line, -1)
-		symbolLocations := symbolFinder.FindAllStringIndex(line, -1)
-		for i, num := range numbers {
-			number, _ := strconv.Atoi(num)
-			parts = append(parts, Part{number, row, numberLocs[i][0], numberLocs[i][1] - 1})
-		}
-		for j, loc := range symbolLocations {
-			location := SymbolLocation{row, loc[0]}
-			data := SymbolData{[]Part{}, symbols[j], location}
-			symbolData = append(symbolData, data)
-		}
+		ParseLine(line, &parts, &symbolData, row)
 		row = row + 1
 	}
 	return parts, symbolData
 }
+
+func ParseLine(line string, parts *[]Part, symbolData *[]SymbolData, row int) {
+	numberFinder := regexp.MustCompile(`[0-9]*`)
+	symbolFinder := regexp.MustCompile(`[^A-Za-z0-9\.]`)
+
+	numbers := numberFinder.FindAllString(line, -1)
+	numberLocs := numberFinder.FindAllStringIndex(line, -1)
+	symbols := symbolFinder.FindAllString(line, -1)
+	symbolLocations := symbolFinder.FindAllStringIndex(line, -1)
+	for i, num := range numbers {
+		number, _ := strconv.Atoi(num)
+		*parts = append(*parts, Part{number, row, numberLocs[i][0], numberLocs[i][1] - 1})
+	}
+	for j, loc := range symbolLocations {
+		location := SymbolLocation{row, loc[0]}
+		data := SymbolData{[]Part{}, symbols[j], location}
+		*symbolData = append(*symbolData, data)
+	}
+}
+
+// ------------------------------------------------------------
+// Part 1
 
 func abs(x int) int {
 	if x < 0 {
@@ -61,15 +69,38 @@ func abs(x int) int {
 	return x
 }
 
+func isAdjacent(part Part, symbol SymbolLocation) bool {
+	withinWidth := ((symbol.col + 1) >= part.start) && ((symbol.col - 1) <= part.end)
+	adjacentRows := (abs(symbol.row-part.row) <= 1)
+	return withinWidth && adjacentRows
+}
+
 func IsPartNumberRelevant(part Part, symbols []SymbolLocation) bool {
 	for _, symbol := range symbols {
-		withinWidth := ((symbol.col + 1) >= part.start) && ((symbol.col - 1) <= part.end)
-		adjacentRows := (abs(symbol.row-part.row) <= 1)
-		if withinWidth && adjacentRows {
+		if isAdjacent(part, symbol) {
 			return true
 		}
 	}
 	return false
+}
+
+// ------------------------------------------------------------
+// Part 2
+
+func AddAdjacentParts(symbol *SymbolData, parts []Part) {
+	for _, part := range parts {
+		if isAdjacent(part, symbol.location) {
+			symbol.adjacentParts = append(symbol.adjacentParts, part)
+		}
+	}
+}
+
+func IsGear(symbol SymbolData) bool {
+	return symbol.symbol == "*" && len(symbol.adjacentParts) == 2
+}
+
+func GetGearRatio(symbol SymbolData) int {
+	return symbol.adjacentParts[0].number * symbol.adjacentParts[1].number
 }
 
 // ------------------------------------------------------------
@@ -84,9 +115,10 @@ func main() {
 
 	parts, symbols := parse(fileScanner)
 	symbolLocations := lo.Map(symbols, func(s SymbolData, _ int) SymbolLocation {
-	    return s.location
+		return s.location
 	})
 
+	// Part 1
 	var partNumbersSum = 0
 	for _, part := range parts {
 		if IsPartNumberRelevant(part, symbolLocations) {
@@ -94,11 +126,17 @@ func main() {
 		}
 	}
 
-	// TODO: Iterate over all symbols, and
-	//  a) count neighbouring numbers
-	//	b) check if "*"
-	//	c) add sum of gear ratio to total if a && b
-	// NB: a and b sound like `GetSymbolData`, `IsGear`, and `GetGearRatio` methods
+	// Part 2
+	var gearRatiosSum = 0
+	for _, symbol := range symbols {
+
+		AddAdjacentParts(&symbol, parts)
+		if IsGear(symbol) {
+			fmt.Println("found a gear")
+			gearRatiosSum = gearRatiosSum + GetGearRatio(symbol)
+		}
+	}
 
 	fmt.Printf("Sum of all the active part numbers: %d\n", partNumbersSum)
+	fmt.Printf("Sum of all the gear ratios: %d\n", gearRatiosSum)
 }
--- gondola/gondola_test.go
+++ gondola/gondola_test.go
@@ -4,6 +4,43 @@ import (
 	"testing"
 )
 
+
+func TestParseLine(t *testing.T) {
+	line := "....123...$.."
+	row := 10
+	parts := []Part{}
+	symbols := []SymbolData{}
+
+	ParseLine(line, &parts, &symbols, row)
+	if len(parts) != 1 {
+		t.Error("Failed to find part")
+	}
+	if parts[0].row != row {
+		t.Errorf("Part placed in wrong row. got: %d\n", parts[0].row)
+	}
+	if parts[0].start != 4 {
+		t.Errorf("Part start incorrect. got: %d\n", parts[0].start)
+	}
+	if parts[0].end != 6 {
+		t.Errorf("Part end incorrect. got: %d\n", parts[0].end)
+	}
+	if len(symbols) != 1 {
+		t.Error("Failed to find symbol")
+	}
+	if symbols[0].symbol != "$" {
+		t.Errorf("Found incorrect symbol. Got: %s\n", symbols[0].symbol)
+	}
+	if symbols[0].location.row != row {
+		t.Errorf("Found incorrect row for symbol. Got: %d\n", symbols[0].location.row)
+	}
+	if symbols[0].location.col != 10 {
+		t.Errorf("Found incorrect column for symbol. Got: %d\n", symbols[0].location.col)
+	}
+}
+
+// ------------------------------------------------------------
+// Part 1
+
 func TestPartWithinWidthIsRelevant(t *testing.T) {
 	part := Part{123, 4, 3, 5}
 	symbolLocation := SymbolLocation{3, 4}
@@ -69,3 +106,98 @@ func TestPartTwoAboveIsNotRelevant(t *testing.T) {
 
 // ------------------------------------------------------------
 // Part 2
+
+func TestFindSingleAdjacentPart(t *testing.T) {
+	parts := []Part{Part{123, 4, 3, 5}}
+	symbol := SymbolData{[]Part{}, "*", SymbolLocation{3, 2}}
+	want := 1
+
+	AddAdjacentParts(&symbol, parts)
+	if len(symbol.adjacentParts) != want {
+		t.Error("Failed to find adjacent part")
+	}
+}
+
+// To make sure the reference isn't overwriting the adjacentParts array
+func TestFindTwoAdjacentParts(t *testing.T) {
+	parts := []Part{Part{123, 4, 3, 5}, Part{333, 2, 1, 3}}
+	symbol := SymbolData{[]Part{}, "*", SymbolLocation{3, 2}}
+	want := 2
+
+	AddAdjacentParts(&symbol, parts)
+	if len(symbol.adjacentParts) != want {
+		t.Error("Failed to find adjacent parts")
+	}
+}
+
+func TestFindTwoOfThreeAdjacentParts(t *testing.T) {
+	parts := []Part{Part{123, 4, 3, 5}, Part{333, 12, 1, 3}, Part{333, 2, 1, 3}}
+	symbol := SymbolData{[]Part{}, "*", SymbolLocation{3, 2}}
+	want := 2
+
+	AddAdjacentParts(&symbol, parts)
+	got := len(symbol.adjacentParts)
+	if got != want {
+		t.Errorf("Failed to find correct number of adjacent parts. Got %d\n", got)
+	}
+}
+
+func TestIsGear(t *testing.T) {
+	symbol := SymbolData{
+		[]Part{Part{123, 4, 3, 5}, Part{333, 2, 1, 3}},
+		"*",
+		SymbolLocation{3, 2},
+	}
+
+	if !IsGear(symbol) {
+		t.Error("Failed to identify gear")
+	}
+}
+
+func TestGearIsWrongSymbol(t *testing.T) {
+	symbol := SymbolData{
+		[]Part{Part{123, 4, 3, 5}, Part{333, 2, 1, 3}},
+		"$",
+		SymbolLocation{3, 2},
+	}
+
+	if IsGear(symbol) {
+		t.Error("Incorrectly identified $ symbol as gear")
+	}
+}
+
+func TestGearHasOnlyOneAdjacentPart(t *testing.T) {
+	symbol := SymbolData{
+		[]Part{Part{123, 4, 3, 5}},
+		"*",
+		SymbolLocation{3, 2},
+	}
+
+	if IsGear(symbol) {
+		t.Error("Incorrectly identified symbol with one ajacent part as gear")
+	}
+}
+
+func TestFindsPartsAndIsGear(t *testing.T) {
+	parts := []Part{Part{123, 4, 3, 5}, Part{333, 2, 1, 3}}
+	symbol := SymbolData{[]Part{}, "*", SymbolLocation{3, 2}}
+
+	AddAdjacentParts(&symbol, parts)
+	if !IsGear(symbol) {
+		t.Error("Failed to find adjacent parts and identify as gear")
+	}
+}
+
+func TestGetGearRatio(t *testing.T) {
+	symbol := SymbolData{
+		[]Part{Part{12, 4, 3, 5}, Part{30, 2, 1, 3}},
+		"*",
+		SymbolLocation{3, 2},
+	}
+	want := 360
+
+	got := GetGearRatio(symbol)
+	if want != got {
+		t.Errorf("Got incorrect gear ratio. Got: %d\n", got)
+	}
+}
