103f270ee40dcb573f963edd5f2df2774fa11f8c
Tom Renner <tommrenner@gmail.com>
2023-12-08 18:21:27 +0100

    Add failing tests for parsing

--- 5-farming/farming_test.go
+++ 5-farming/farming_test.go
@@ -15,6 +15,38 @@ func TestParseSeedsInput(t *testing.T) {
 	}
 }
 
+func TestParseMappeTitleLine(t *testing.T) {
+	line := "water-to-light map:"
+	wantSource := "water"
+	wantDestination := "light"
+
+	gotSource, gotDestination := ParseMappeTitleLine(line)
+	if gotSource != wantSource {
+		t.Errorf("Failed to parse Mappe source. Got: %s\n", gotSource)
+	}
+	if gotDestination != wantDestination {
+		t.Errorf("Failed to parse Mappe destination. Got: %s\n", gotDestination)
+	}
+}
+
+func TestParseMappeDirectiveLine(t *testing.T) {
+	line := "12 13 14"
+	wantSourceStart := 13
+	wantDestinationStart := 12
+	wantRangeLength := 14
+
+	source, destination, length := ParseMappeDirectiveLine(line)
+	if source != wantSourceStart {
+		t.Errorf("Failed to parse Mappe source range start. Got: %d\n", source)
+	}
+	if destination != wantDestinationStart {
+		t.Errorf("Failed to parse Mappe destination range start. Got: %d\n", destination)
+	}
+	if length != wantRangeLength {
+		t.Errorf("Failed to parse Mappe range length. Got: %d\n", length)
+	}
+}
+
 // ------------------------------------------------------------
 // Part 1
 
