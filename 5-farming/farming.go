4c6b391b30d80b6e89a4af2569030957f38e2f6f
Tom Renner <tommrenner@gmail.com>
2023-12-08 18:22:38 +0100

    Go fmt

--- 5-farming/farming.go
+++ 5-farming/farming.go
@@ -7,55 +7,53 @@ import (
 	"strings"
 )
 
-
 type Mappe struct {
 	directives []Directive
-	source string
+	source     string
 	desination string
 }
 
 type Directive struct {
-	sourceRangeStart int
+	sourceRangeStart      int
 	destinationRangeStart int
-	rangeLength int
+	rangeLength           int
 }
-
 
 func parse(fileScanner *bufio.Scanner) ([]int, map[string]Mappe) {
 	mapOfMappes := make(map[string]Mappe)
-    var seeds []int
+	var seeds []int
 
-    var currentMappe Mappe
-    mappeTitleLine := regexp.MustCompile(`^[a-z]+`)
-    mappeDirectiveLine := regexp.MustCompile(`^[0-9]+`)
+	var currentMappe Mappe
+	mappeTitleLine := regexp.MustCompile(`^[a-z]+`)
+	mappeDirectiveLine := regexp.MustCompile(`^[0-9]+`)
 
-    for fileScanner.Scan() {
-    	line := strings.Trim(fileScanner.Text(), " ")
-    	if strings.HasPrefix(line, "seeds:") {
-    		seeds = ParseSeedsInputLine(line)
-    	} else if line == "" && currentMappe.source != "" {
-    		mapOfMappes[currentMappe.source] = currentMappe
-    	} else if mappeTitleLine.MatchString(line) {
-    		source, destination := ParseMappeTitleLine(line)
-    		currentMappe = Mappe{
-    			[]Directive{},
-    			source,
-    			destination,
-    		}
-    	} else if mappeDirectiveLine.MatchString(line) {
-    		source, destination, length := ParseMappeDirectiveLine(line)
-    		directive := Directive{
-    			source,
-    			destination,
-    			length,
-    		}
-    		currentMappe.directives = append(currentMappe.directives, directive)
-    	}
-    }
-    // No newline at end of file --> EOF ends the final Mappe
+	for fileScanner.Scan() {
+		line := strings.Trim(fileScanner.Text(), " ")
+		if strings.HasPrefix(line, "seeds:") {
+			seeds = ParseSeedsInputLine(line)
+		} else if line == "" && currentMappe.source != "" {
+			mapOfMappes[currentMappe.source] = currentMappe
+		} else if mappeTitleLine.MatchString(line) {
+			source, destination := ParseMappeTitleLine(line)
+			currentMappe = Mappe{
+				[]Directive{},
+				source,
+				destination,
+			}
+		} else if mappeDirectiveLine.MatchString(line) {
+			source, destination, length := ParseMappeDirectiveLine(line)
+			directive := Directive{
+				source,
+				destination,
+				length,
+			}
+			currentMappe.directives = append(currentMappe.directives, directive)
+		}
+	}
+	// No newline at end of file --> EOF ends the final Mappe
 	mapOfMappes[currentMappe.source] = currentMappe
 
-    return seeds, mapOfMappes
+	return seeds, mapOfMappes
 }
 
 func ParseSeedsInputLine(line string) []int {
@@ -87,7 +85,6 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	// seeds, mapOfMappes := parse(fileScanner)
-
 
 	// fmt.Printf("Lowest location number for initial seeds: %d\n", lowestLocation)
 }
